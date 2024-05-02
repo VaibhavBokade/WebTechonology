628aa6d0cd4aa77f4c01d59fbeb7ccc6f8edbef9
Vaibhav Bokade <vaibhavbokade112@gmail.com>
2024-05-02 15:11:17 +0530

    All html,css and javascript codes

---
+++ module.js
@@ -0,0 +1,4 @@
+export let a=10
+
+export let show=()=>"hello from show function of module.js file"
+export let add=(a,b)=>a+b
---
+++ module1.js
@@ -0,0 +1,5 @@
+
+import {a,show,add} from './module.js'
+console.log(show()) 
+console.log(a)
+console.log(add(2,3))
